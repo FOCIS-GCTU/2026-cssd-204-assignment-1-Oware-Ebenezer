86fd075e7d69fd421f8593b7d1640f71aae21b7c
Ebenezer Oware Kwabena <nanaprestige71@gmail.com>
2026-05-30 09:59:32 +0000

    Submit assignment: 1, Scripting Language

--- initials.py
+++ initials.py
@@ -1,6 +1,33 @@
+#File: initials.py
+#Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Ebenezer Kwabena Oware
+#STUDENT ID: 2425402268
+#Email: 2425402268@live.gctu.edu.gh
+#Grader: Augustus
+#
+# On my honor, Ebenezer Kwabena Oware, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+  #Code to print out my initials EKO
+
+  print("print out my initials\n\n")
+
+  print("...EKO\n\n")
+
+  print("...EEEEEEEEEEEEE........KKKKK......KKK........OOOOOOOOOOOOOOOOO...")
+  print("...EEEEEEEEEEEEE........KKKKK.....KKK.........O.O.............O.O.....")
+  print("...EEEEEE...............KKKKK....KKK..........O.O.............O.O.....")
+  print("...EEEEEE...............KKKKK...KKK...........O.O.............O.O.....")
+  print("...EEEEEEEEEEEEE........KKKKKKKK..............O.O.............O.O.....")
+  print("...EEEEEEEEEEEEE........KKKKKKKK..............O.O.............O.O.....")
+  print("...EEEEEE...............KKKKK....KKK..........O.O.............O.O.....")
+  print("...EEEEEE...............KKKKK.....KKK.........O.O.............O.O.....")
+  print("...EEEEEEEEEEEEE...**...KKKKK......KKKK...**...O.O.............O.O...**")
+  print("...EEEEEEEEEEEEE...**...KKKKK.......KKK...**...OOOOOOOOOOOOOOOOO....**\n\n")
 
 
 
